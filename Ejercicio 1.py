bff446f31db8644b779e7547b09e41d8af407b44
aoderizlav <113341236+aoderizlav@users.noreply.github.com>
2022-11-28 11:30:36 +0100

    Examen

---
+++ Ejercicio 1.py
@@ -0,0 +1,28 @@
+def convert_rgb2gray (lista_pixeles):
+    """
+    Esta funcion convierte una lista de pixeles de espectro de color (RGB) a ota lista de pixeles en escala de grises
+    Parametros de entrada:
+    -lista: Contendra un numero indeterminado de tuplas con 3 elementos
+    Salidas:
+    -Lista de valores de luminancia (Y)
+    """
+    lista = ["RN ,GN ,BN"]
+    valores_RGB = [0 ,255 ]
+    tupla = []
+    for parametro in lista:
+        print(int(num(lista)))
+
+
+
+
+
+
+
+def rgb2gray (lista_pixeles):
+    """
+    Esta funcion hace el calculo de la componete de luminancia
+    Parametros de entrada:
+    -Variables de tipo int E,G Y B
+    Salidas:
+    -Variable de tipo int Y
+    """
